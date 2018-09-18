6bcfe05e54bfb09fda85ee0afb9eb9f6587bc1e4
Jose1601 <jose.bustamante9@unmsm.edu.pe>
2018-09-17 23:44:02 -0500

    Solución con Hibernate y Jar-Deployment

---
+++ src/main/java/com/pe/sercosta/scks/ScksApplication.java
@@ -0,0 +1,12 @@
+package com.pe.sercosta.scks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScksApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pe/sercosta/scks/ScksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pe.sercosta.scks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
