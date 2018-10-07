a866b02b302772e62acb794db3c640378eca5465
Miguel <sacredgg@gmail.com>
2018-10-06 21:03:38 -0500

    Agregada Interface O5

--- src/main/java/com/pe/sercosta/scks/repositories/IContenidoRepository.java
+++ src/main/java/com/pe/sercosta/scks/repositories/IContenidoRepository.java
@@ -1,9 +1,20 @@
 package com.pe.sercosta.scks.repositories;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+
 import com.pe.sercosta.scks.entities.Contenido;
 
 public interface IContenidoRepository {
+	
+	
+	/**
+	 * Lista los contenidos
+	 * @param sesion
+	 * @return List<Presentacion>
+	 */
+	public abstract List<Contenido> listarContenidos(EntityManager sesion);
 	
 	/**
 	 * Registra un contenido
@@ -12,5 +23,7 @@ public interface IContenidoRepository {
 	 * @param contenido
 	 */
 	public abstract void registrarContenido(EntityManager sesion, Contenido contenido);
+	
+
 	
 }
