95849e6da14146f369ce86ffe8d3aa0bc30e0cec
Miguel <sacredgg@gmail.com>
2018-10-08 10:16:27 -0500

    Corregido Laboratorio-Service

--- src/main/java/com/pe/sercosta/scks/services/implementation/LaboratorioService.java
+++ src/main/java/com/pe/sercosta/scks/services/implementation/LaboratorioService.java
@@ -14,6 +14,7 @@ import org.apache.commons.logging.LogFactory;
 import com.pe.sercosta.scks.entities.Laboratorio;
 import com.pe.sercosta.scks.exceptions.SercostaException;
 import com.pe.sercosta.scks.repositories.IContenidoRepository;
+import com.pe.sercosta.scks.repositories.ILaboratorioRepository;
 
 public class LaboratorioService implements ILaboratorioService{
 
